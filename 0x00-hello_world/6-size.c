3a78110ef7a71aa6424537a06c083f3f73f5a1b2
codyempress <codyempress@gmail.com>
2023-10-05 09:47:28 -0700

    More C Basics

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the size of various types on the computer it is compiled and run on
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 bytes\n");
+	return (0);
+}
