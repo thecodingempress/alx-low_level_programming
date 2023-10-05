ba34ef442d83b03c6541078c3659deac3db40f0c
codyempress <codyempress@gmail.com>
2023-10-05 09:11:00 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include stdio.h
+
+/*
+ * prints out a string with 'Puts' function and returns 0
+ *
+ */
+
+int main ()
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0
+}
