1e156f43cd392e22d4c7140bbdb8b01e4688fb47
codyempress <codyempress@gmail.com>
2023-10-11 11:39:54 -0700

    Edits

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two numbers and returns the result
+ * @nu1:1st number
+ * @nu2: 2nd num
+ *
+ * Return: sum of numbers
+ */
+
+int add(int nu1, int nu2)
+{
+	int sum = nu1 + nu2;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all numbers between n and 98
+ * @n: number
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		printf("%d\n", 98);
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		printf("%d\n", 98);
+	}
+}
