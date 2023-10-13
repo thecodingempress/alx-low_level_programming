2eee9bf29a8b87ae14209271c95c18fb3eb47cfb
codyempress <codyempress@gmail.com>
2023-10-13 05:16:03 -0700

    Function to test afunction

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests if the functions fives teh correct output
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
