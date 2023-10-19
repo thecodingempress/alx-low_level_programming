3957deaee76d2a493a2cd999ecbaf7278ea2cc69
codyempress <codyempress@gmail.com>
2023-10-19 19:36:43 +0300

    edits

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,4 +9,6 @@
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 != 0 || *s2 != '\0')
-		if (*
+	{
+	}
+}
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reverse_array - reverse the elements of an array
+ * @a: array
+ * @n: number of array elements
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	while (n > 0)
+	{
+		*a = a[n-1];
+		n++;
+		a++;
+	}
+}
