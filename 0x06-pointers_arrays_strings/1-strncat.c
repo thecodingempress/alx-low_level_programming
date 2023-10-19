3a76f76a555ffe451d75f3a03e6748abd40ca6cd
codyempress <codyempress@gmail.com>
2023-10-19 17:41:52 +0300

    Edits

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - adds n bytes of src to dest
+ * @src: source
+ * @dest: destination
+ * @n: number of chars to append
+ *
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (i < n)
+	{
+		*dest = src[i];
+		i++;
+	}
+	return (dest);
+}
