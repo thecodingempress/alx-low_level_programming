a85a441740a4e6635ea620b0af8e3a26184e804c
codyempress <codyempress@gmail.com>
2023-10-30 20:24:57 +0300

    Edits

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,4 +10,38 @@
  */
 int **alloc_grid(int width, int height)
 {
+	int **new;
+	int i,j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	new = (int **) malloc(height * sizeof(int *));
+	
+	if (new == NULL)
+	{
+		return NULL;
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		new[i] = (int *) malloc(width * sizeof(int));
+	}
+	if (new[i] == NULL)
+	{
+		for (j = 0; j < i; j++)
+		{
+			free(new[j]);
+		}
+		free(new);
+		return (NULL);
+	}
+
+	for (j = 0; j < width; j++)
+	{
+		new[i][j] = 0;
+	}
+	return (new);
 }
