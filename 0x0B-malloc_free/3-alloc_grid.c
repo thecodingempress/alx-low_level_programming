4edfc059618dfdae577ca1ac410d843e74ae30e7
codyempress <codyempress@gmail.com>
2023-10-31 15:31:36 +0300

    Edits

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,47 +1,43 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 #include <stddef.h>
 /**
- * alloc_grid - returns a pointer to a 2-D array of ints;
- * @width: first column
- * @height: second column
+ * alloc_grid - creates memory for two-dimensional array
+ * @width: width of array
+ * @height: of array
  *
- * Return: a pointer to a 2D array
+ * Return: pointer to start of array
  */
 int **alloc_grid(int width, int height)
 {
 	int **new;
-	int i,j;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 
-	new = (int **) malloc(height * sizeof(int *));
-	
+	new  = (int **)malloc(sizeof(int) * height);
+
 	if (new == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	for (i = 0; i < height; i++)
 	{
-		new[i] = (int *) malloc(width * sizeof(int));
-	}
-	if (new[i] == NULL)
-	{
-		for (j = 0; j < i; j++)
-		{
-			free(new[j]);
-		}
-		free(new);
-		return (NULL);
+		new[i] = (int *)malloc(width * sizeof(int));
 	}
 
-	for (j = 0; j < width; j++)
+	for (i = 0; i < height; i++)
 	{
-		new[i][j] = 0;
+		for (j = 0; j < width; j++)
+		{
+			new[i][j] = 0;
+		}
 	}
+
 	return (new);
+
 }
