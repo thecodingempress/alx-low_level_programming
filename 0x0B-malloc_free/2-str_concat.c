89d808883d55e173ab3f6ae9877c8c9e5c4e9538
codyempress <codyempress@gmail.com>
2023-10-31 14:30:11 +0300

    Edits

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,8 @@ char *str_concat(char *s1, char *s2)
 	int l = strlen(s1) + strlen(s2);
 	int j = strlen(s1);
 	char *new;
-	int i, m = 0;
+	int i = 0;
+	unsigned int m = 0;
 
 	if (s1 == NULL)
 	{
@@ -25,7 +26,12 @@ char *str_concat(char *s1, char *s2)
 		s2 = " ";
 	}
 
-	new  = (char *) malloc(sizeof(char) * l);
+	new  = (char *) malloc(sizeof(char) * (l + 1));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 
 	while (i < j)
 	{
@@ -33,18 +39,13 @@ char *str_concat(char *s1, char *s2)
 		i++;
 	}
 
-	while (i < l)
+	while (i < l && m < strlen(s2))
 	{
 		new[i] = s2[m];
 		i++;
 		m++;
 	}
 
-	new[i] = '\0';
-
-	if (new == NULL)
-	{
-		return (NULL);
-	}
+	new[l] = '\0';
 	return (new);
 }
