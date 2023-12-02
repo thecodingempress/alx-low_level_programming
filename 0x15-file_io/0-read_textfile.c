295f693bc587c6a6b88fc192785fc6a740bf1f06
codyempress <codyempress@gmail.com>
2023-12-02 16:42:39 +0300

    Open, read, write

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+/**
+ * read_textfile - reads a text fie and prints it to POSIX
+ * standard output
+ * @filename: name of file in question
+ * @letters: number of letters it should read and print
+ *
+ * Return: teh actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t i, k;
+	int j;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	buf = malloc(sizeof(char) * letters);
+
+	if (buf == NULL)
+	{
+		return (0);
+	}
+	j = open (filename, O_RDONLY);
+
+	if (j == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	i = read(j, buf, letters);
+	
+	if (i == -1)
+	{
+		close (j);
+		free (buf);
+		return (0);
+	}
+	k = write(1, buf, i);
+
+	if (k == -1)
+	{
+		close (j);
+		free(buf);
+		return (0);
+	}
+
+	close(j);
+	free(buf);
+	return (k);
+}
