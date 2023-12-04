f5d56e3e9d5262566f85b884156b7734096ba1bc
codyempress <codyempress@gmail.com>
2023-12-04 14:32:33 +0300

    Edits

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	j = open(filename, O_CREAT | O_RDWR, 0600);
+	j = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (j == -1)
 	{
 		return (-1);
