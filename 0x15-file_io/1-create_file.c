e81e2d9dd2845148bd7a292854bc9102b963cdc6
codyempress <codyempress@gmail.com>
2023-12-04 14:25:57 +0300

    Edits

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+/**
+ * create_file - creates a file
+ * @filename: pointer to filename
+ * @text_content: a null-terminatedtring to be
+ * written in the file created
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int j, l;
+	ssize_t i;
+
+	j = open(filename, O_CREAT | O_RDWR);
+	if (j == -1)
+	{
+		return (-1);
+	}
+
+	l = 0;
+	while (text_content[l] != '\0')
+	{
+		l++;
+	}
+
+	i = write(j, text_content, l);
+	if (i == -1)
+	{
+		close(j);
+		return (-1);
+	}
+
+	close(j);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,5 +3,6 @@
 #include <stddef.h>
 #include <sys/types.h>
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
