4a328ad06ef376f57b8b93e795bf63bfcd5734e1
codyempress <codyempress@gmail.com>
2023-12-04 14:56:23 +0300

    Append to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,6 +21,11 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+
 	len = 0;
 	while (text_content[len] != '\0')
 	{
